d77e61bf31d8161b530960b918a4e724f2afb1d4
NguyenLong <long04dz@gmail.com>
2024-09-01 21:32:19 +0700

    maybay

---
+++ app.js
@@ -0,0 +1,24 @@
+// Try edit me
+
+function countdown() {
+    let count = 0;
+    let count2 = 0;
+    const interval = setInterval(() => {
+        const seconds = count.toString().padStart(2, '0');
+        const milliseconds = count2.toString().padStart(1, '0');
+        //console.log(`${seconds}:${milliseconds}`);
+        a = `${seconds}:${milliseconds} X`;
+        document.getElementById('header').innerText = a;
+        count2++;
+        if (count2 > 100){
+          count2 = 0;
+          count++;
+        }
+        if (count > 5) {
+            clearInterval(interval);
+            setTimeout(countdown, 3000); // Wait for 1 millisecond before starting again
+        }
+    }, 40); // 1 millisecond interval
+}
+
+//countdown()
